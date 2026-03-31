8543ed090169a4930ffb3dc125354b40b35acd3c
thislifeeyelead <thislife.eyelead@gmail.com>
2026-03-31 02:47:42 -0600

    Create App component to render Dashboard and CalendarView

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import CalendarView from "./components/CalendarView";
+
+export default function App() {
+  return (
+    <div>
+      <Dashboard />
+      <CalendarView />
+    </div>
+  );
+}
